332ec2ba5ad1c381a6268110b35308b53bea7261
tianshuo <qwe321a3@126.com>
2019-08-26 10:38:24 +0800

    折半/二分算法实例 修改类名称

--- java-struct/src/main/java/com/jvm/order/HalfSearch.java
+++ java-struct/src/main/java/com/jvm/order/HalfSearch.java
@@ -4,8 +4,14 @@ package com.jvm.order;
 /**
  * 折半算法 / 2分算法
  * 找到某个值得索引位置。
+ * 思路：
+ * 1、定义头指针，定义尾指针
+ * 2、计算中间值 mid = (头指针+尾指针)/2
+ * 3、要查找的值和mid索引对应的值做比较，如果大于mid索引出值，则舍弃左侧值，修改头指针索引位mid+1; 如果小于mid索引处值，则修改的尾部
+ * 指针为mid-1;
+ * 4、然后只要头部指针小于或者等于尾部指针就继续按照1-3步继续查找。
  */
-public class HalfOrder {
+public class HalfSearch {
 
 
     private static final int [] arrs = new int[]{1,10,13,18,31,40,58,73,90};
