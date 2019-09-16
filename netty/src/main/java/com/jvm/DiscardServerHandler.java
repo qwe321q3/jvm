7abc7fe8d1b0c37df6a757671858be9c4109a6b3
tianshuo <qwe321a3@126.com>
2019-09-16 11:19:22 +0800

    增加netty测试例子

---
+++ netty/src/main/java/com/jvm/DiscardServerHandler.java
@@ -0,0 +1,34 @@
+package com.jvm;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+//        super.channelRead(ctx, msg);
+
+            ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) { // (1)
+                System.out.print((char) in.readByte());
+                System.out.flush();
+            }
+        } finally {
+            ReferenceCountUtil.release(msg); // (2)
+        }
+
+//        System.out.println("接收消息:"+msg);
+//
+//        ((ByteBuf)msg).release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
