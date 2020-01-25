18f6b34aa2486f7e4fc8c2efc6784f06c71e5c62
tianshuo <qwe321a3@126.com>
2020-01-25 19:18:24 +0800

    修改运行时jvm参数配置，增加gc日志打印，设置栈空间-Xss180k

--- jvm-introduction/src/main/java/com/jvm/vmstack/LocalVariable.java
+++ jvm-introduction/src/main/java/com/jvm/vmstack/LocalVariable.java
@@ -1,13 +1,14 @@
 package com.jvm.vmstack;
 
 /**
+ * 测试机器 Mac os  i9 32G内存
  * Virtual Manchine Stack --> stack frame --> 局部变量表 (LocalVariable)
  * <p>
  * 每次函数调用都会生成对应的栈帧，因为栈帧中有局部变量，操作数栈，动态链接，方法的返回地址
  * stack frame 对应类中的方法
  * 用于存放栈帧中的局部变量
  * <p>
- * 虚拟机栈可以用-Xss150k 参数，来设置栈的大小
+ * 虚拟机栈可以用-Xss180k 参数，来设置栈的大小
  * <p>
  * 此类用来设置局部变量是和Virtual Machine Stack相关的，测试有参数，有局部变量及无参数方法
  * 在相同栈大小的情况下，调用深度不一样。
@@ -31,21 +32,21 @@ public class LocalVariable {
     }
 
     public static void main(String[] args) {
-//        try {
-//            alloc(1,2);
-//
-//        }catch (Throwable e){ //310次
-//            System.out.println("有参数方法调用次数: "+count);
-//            e.printStackTrace();
-//        }
-
         try {
-            alloc1();
+            alloc(1,2);
 
-        } catch (Throwable e) {
-            e.printStackTrace(); //546次
-            System.out.println("无参数方法调用次数: " + count);
+        }catch (Throwable e){ //582次
+            System.out.println("有参数方法调用次数: "+count);
+            e.printStackTrace();
         }
+
+//        try {
+//            alloc1();
+//
+//        } catch (Throwable e) {
+//            e.printStackTrace(); //1106次
+//            System.out.println("无参数方法调用次数: " + count);
+//        }
     }
 
 
