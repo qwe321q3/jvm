cceb293329fb899eca35668673ccc9d06d76966b
tianshuo <qwe321a3@126.com>
2020-02-04 20:58:54 +0800

    增加<clinit>实例方法，<clinit>是由静态成员赋值和static静态块合并生成的，如果类中没有的静态成员赋值方法和static静态块的那么jvm就不会给此类生成<clinit>块

--- jvm-introduction/src/main/java/com/jvm/banarycode/MyTest8.java
+++ jvm-introduction/src/main/java/com/jvm/banarycode/MyTest8.java
@@ -5,8 +5,24 @@ package com.jvm.banarycode;
  * 解释执行，就是通过解释器解释字节码，遇到相应指令就去执行
  * 编译执行，就是通过JIT(Just In Time)即时编译器将字节码转换为机器码去执行，现代的JVM会通过代码热点
  * 来生成相应的本地机器码
+ *
+ * <clinit> 方法静态成员和静态块合并构成的
+ * initial
+ * <p>
+ * 如果类中不存在静态成员赋值或者静态块，那么的jvm不会给类生成<clinit> 方法
  */
 public class MyTest8 {
+//
+//    private static String a = "a";
+//    private static int b;
+//    会生成<clinit>
+//    static {
+//        b = 2;
+//    }
+
+    //不会生成<clinit>方法
+    private static final String b = "d";
+    private static final int a = 50;
 
 
 }
