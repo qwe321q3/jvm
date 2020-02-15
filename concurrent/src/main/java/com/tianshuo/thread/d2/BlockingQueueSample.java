7702885f435a028f9601d3de0364dc5ea62769b9
tianshuo <qwe321a3@126.com>
2020-02-15 17:50:25 +0800

    同步容器，同步阻塞容器实例，后续增加线程池实例

--- concurrent/src/main/java/com/tianshuo/thread/d2/BlockingQueueSample.java
+++ concurrent/src/main/java/com/tianshuo/thread/d2/BlockingQueueSample.java
@@ -1,4 +1,43 @@
 package com.tianshuo.thread.d2;
 
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class BlockingQueueSample {
+    public static void main(String[] args) {
+        LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<>();
+
+        new Thread(()->{
+            for (int i = 0; i < 10; i++) {
+                try {
+                    queue.put("aa"+i);
+                    System.out.println(Thread.currentThread().getName());
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        new Thread(()->{
+            while(true){
+                try {
+                    String take = queue.take();
+                    if(take!=null){
+                        System.out.println(take);
+                    }else{
+                        break;
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }).start();
+        System.out.println("ppppp");
+
+
+    }
+
+
 }
