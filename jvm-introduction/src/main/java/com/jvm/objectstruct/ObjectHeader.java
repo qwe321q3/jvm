7666ef75a86af8723969d19e887a6927c620e7ec
tianshuo <qwe321a3@126.com>
2020-03-15 21:59:12 +0800

    增加JOL的jar学习的对象的内存布局。尤其的是对象头问题 对象内存= 对象头(mark word + class pointer) + 对象实例信息 + 填充信息。

--- jvm-introduction/src/main/java/com/jvm/objectstruct/ObjectHeader.java
+++ jvm-introduction/src/main/java/com/jvm/objectstruct/ObjectHeader.java
@@ -1,13 +1,25 @@
 package com.jvm.objectstruct;
 
+import org.openjdk.jol.info.ClassLayout;
+
 /**
  * 使用openJDK 提供的JOL的来打印对象头
  *
- *  对象头= mark word + class pointer(指针) 只想方法区的类元数据
+ *  对象头= mark word(固定 8字节) + class pointer(指针) 只想方法区的类元数据
  *
- *
- *
+ *  对象的大小，刚好要被8整除，如果不够8整除，jvm会把填充的到能够被8整除
  *
  */
 public class ObjectHeader {
+
+    public static void main(String[] args) {
+        Object object = new Object();
+        String layout = ClassLayout.parseInstance(object).toPrintable();
+
+        System.out.println(layout);
+        synchronized (object) {
+            System.out.println(layout);
+        }
+
+    }
 }
