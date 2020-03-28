e6b181782e7bdc34add776031b4ae268e2d82f11
tianshuo <qwe321a3@126.com>
2020-03-28 23:09:08 +0800

    修改代码位置

--- jvm-introduction/src/main/java/com/jvm/objectstruct/ObjectHeader.java
+++ jvm-introduction/src/main/java/com/jvm/objectstruct/ObjectHeader.java
@@ -9,7 +9,7 @@ import org.openjdk.jol.info.ClassLayout;
  *
  *  对象的大小，刚好要被8整除，如果不够8整除，jvm会把填充的到能够被8整除
  *
- */;
+ */
 public class ObjectHeader {
 
     public static void main(String[] args) {
@@ -19,6 +19,10 @@ public class ObjectHeader {
         System.out.println(layout);
         synchronized (object) {
             System.out.println(layout);
+        }
+
+        for (int i = 0; i <1000_1000 ; i++) {
+
         }
 
     }
