bef7fff72f0f1d63edc847c7db5acfea5678e66b
tianshuo <qwe321a3@126.com>
2020-06-02 17:56:28 +0800

    增加重排序测试类

--- concurrent/src/main/java/com/tianshuo/thread/lock/ReOrder.java
+++ concurrent/src/main/java/com/tianshuo/thread/lock/ReOrder.java
@@ -4,7 +4,7 @@ package com.tianshuo.thread.lock;
  * 线程重排序实验
  * <p>
  * 重排序是为了刚好的使用多核CPU，CPU会在对代码语义没有影响的情况下，对代码进行重排序。  使代码在多线程环境中，使代码执行在多线程环境
- * 中看起来像是有序一下。  as-ifself-serial
+ * 中看起来像是有序一下。  as-if-serial
  */
 public class ReOrder {
     static int a = 0, b = 0;
@@ -13,9 +13,11 @@ public class ReOrder {
     public static void main(String[] args) throws InterruptedException {
 
         int i = 0;
-        for (; ; ) {
+        while (true ) {
 
             Thread t1 = new Thread(() -> {
+                //由于线程one先启动，下面这句话让它等一等线程two. 可根据自己电脑的实际性能适当调整等待时间.
+//                shortWait(10000);
                 a = 1;
                 x = b;
             }, "t1");
@@ -30,6 +32,7 @@ public class ReOrder {
             t1.join();
             t2.join();
             i++;
+            System.out.println("第" + i + "次 : x="+x+" ,y="+y);
 
             if (x == 0 && y == 0) {
                 System.out.println("第" + i + "次 : x=0 ,y=0");
@@ -39,5 +42,13 @@ public class ReOrder {
 
         }
 
+    }
+
+    public static void shortWait(long interval){
+        long start = System.nanoTime();
+        long end;
+        do{
+            end = System.nanoTime();
+        }while(start + interval >= end);
     }
 }
