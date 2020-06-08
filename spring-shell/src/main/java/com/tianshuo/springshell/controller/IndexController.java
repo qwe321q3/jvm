e3385e28a2cd215b10037b47843fc2dc0af02e5c
tianshuo <qwe321a3@126.com>
2020-06-08 15:28:43 +0800

    增加测试程序，后面增加spring-shell测试方法，测试java-cli

---
+++ spring-shell/src/main/java/com/tianshuo/springshell/controller/IndexController.java
@@ -0,0 +1,36 @@
+package com.tianshuo.springshell.controller;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 测试Controller是否是单例
+ * 默认controller是单例
+ * 设置Scope = request之后的，为多例
+ * @author tianshuo
+ */
+//@Scope("request")
+@Controller
+public class IndexController {
+
+    int a = 0;
+
+    static int b = 0;
+    @ResponseBody
+    @RequestMapping("show")
+    public Map<String,Integer> show() {
+        HashMap<String, Integer> map = new HashMap<>();
+        a++;
+        b++;
+
+        map.put("a", a);
+        map.put("b", b);
+
+        return map;
+    }
+}
