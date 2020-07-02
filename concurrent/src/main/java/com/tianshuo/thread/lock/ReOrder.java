90bc111798565a526578f7ba34dd83c0c6ce7309
tianshuo <qwe321a3@126.com>
2020-07-02 10:47:09 +0800

    指令重排序

--- concurrent/src/main/java/com/tianshuo/thread/lock/ReOrder.java
+++ concurrent/src/main/java/com/tianshuo/thread/lock/ReOrder.java
@@ -3,8 +3,11 @@ package com.tianshuo.thread.lock;
 /**
  * 线程重排序实验
  * <p>
- * 重排序是为了刚好的使用多核CPU，CPU会在对代码语义没有影响的情况下，对代码进行重排序。  使代码在多线程环境中，使代码执行在多线程环境
+ * 重排序是为了刚好的使用多核CPU，CPU会在单个线程在对代码语义没有影响的情况下，对代码进行重排序。  使代码在多线程环境中，使代码执行在多线程环境
  * 中看起来像是有序一下。  as-if-serial
+ *
+ * 实验在第177333次 : x=0 ,y=0
+ * 证实在单线程中会对代码语义没有影响的情况下，进行指令重排序
  */
 public class ReOrder {
     static int a = 0, b = 0;
@@ -14,10 +17,12 @@ public class ReOrder {
 
         int i = 0;
         while (true ) {
+             a = 0; b = 0;
+             x = 0; y = 0;
 
             Thread t1 = new Thread(() -> {
                 //由于线程one先启动，下面这句话让它等一等线程two. 可根据自己电脑的实际性能适当调整等待时间.
-//                shortWait(10000);
+                shortWait(10000);
                 a = 1;
                 x = b;
             }, "t1");
