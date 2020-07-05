ef70acd1d8930b1f01fde1ae2c083d3398fe1b56
tianshuo <qwe321a3@126.com>
2020-07-05 21:08:08 +0800

    增加arthas测试类及泛型擦除理解

--- jvm-introduction/src/main/java/com/jvm/system/EliminateGenericTypes.java
+++ jvm-introduction/src/main/java/com/jvm/system/EliminateGenericTypes.java
@@ -10,9 +10,11 @@ import java.util.List;
  *
  * 关于泛型变量的使用，是会在编译之前检查的
  *
- * 泛型会在编译期间，类型会擦除， List<Integer> list = new ArrayList(); 其实会变成
+ * 泛型在编译期间，类型会擦除，
+ * List<Integer> list = new ArrayList(); 其实会变成
  *         List list = new ArrayList<>();
  *         泛型会被擦除，这时候list只是原始类型，即Object类型
+ *
  * 使用反射可以加入泛型规定外的数据。
  *
  * 重点：
@@ -36,11 +38,28 @@ public class EliminateGenericTypes {
         ArrayList list2 = new ArrayList<String>();
         list2.add("2");
         list2.add(1);
+
+        //泛型检查是针对引用的
+        ArrayList<String> list3 = new ArrayList(); //第一种 情况
+        ArrayList list4 = new ArrayList<String>(); //第二种 情况  泛型检查无效
+
+        list3.add("sss");
+//        list3.add(111);
+
+        list4.add(111);
+
+
     }
 
     /**
      * 当静态方法返回值为泛型类型时，要使用<T>
-     * 泛型方法使用T 是参数指定的T
+     * 泛型方法使用T 是方法参数指定的T，而不是类上指定的T
+     *
+     * 因为泛型类中的泛型参数的实例化是在定义对象的时候指定的，而静态变量和静态方法不需要使用对象来调用
+     *
+     * 1、因为泛型是加在引用上的，静态方法的不会通过引用调用，是通过类直接调用的，
+     * 2、所有必须执行返回参数泛型<T>
+     *
      * @param <T>
      * @return
      */
