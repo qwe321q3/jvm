89850092a08d7c0c68e225d2fb92a4f8586ed654
tianshuo <qwe321a3@126.com>
2020-07-12 21:51:37 +0800

    提交Consumer内容

--- rocketmq/src/main/java/com/jvm/Consumer.java
+++ rocketmq/src/main/java/com/jvm/Consumer.java
@@ -7,16 +7,30 @@ import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
 
 import java.util.List;
 
 public class Consumer {
     public static void main(String[] args) throws MQClientException {
 
-        DefaultMQPushConsumer defaultMQPushConsumer = new DefaultMQPushConsumer("consumerGroup");
-        defaultMQPushConsumer.setNamesrvAddr("192.168.140.133:9876");
+        DefaultMQPushConsumer defaultMQPushConsumer = new DefaultMQPushConsumer("bb");
+        defaultMQPushConsumer.setNamesrvAddr("192.168.31.3:9876;192.168.31.4:9876");
         defaultMQPushConsumer.subscribe("RocketTopic", "");
         defaultMQPushConsumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+        /**
+         * 广播 消费全量消息
+         * 如果有10条消息 ，2个消费端 ，那么2个消费端共同消费这10条消息
+         */
+//        defaultMQPushConsumer.setMessageModel(MessageModel.BROADCASTING);
+
+        /**
+         * 集群 默认集群是集群模式
+         *
+         * 如果有10条消息 ，2个消费端  ，那么2个消费端，每人都会消费10条消息
+         */
+        defaultMQPushConsumer.setMessageModel(MessageModel.BROADCASTING);
+
 
         defaultMQPushConsumer.registerMessageListener(new MessageListenerConcurrently() {
             @Override
