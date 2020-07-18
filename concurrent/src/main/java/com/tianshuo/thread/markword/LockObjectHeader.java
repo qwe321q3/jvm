d5f9cc3bfa305d76ac0520aaabbeb24d0ec51bfd
tianshuo <qwe321a3@126.com>
2020-07-18 15:20:23 +0800

    增加joljar，增加不同锁情况下的对象头打印。

---
+++ concurrent/src/main/java/com/tianshuo/thread/markword/LockObjectHeader.java
@@ -0,0 +1,26 @@
+package com.tianshuo.thread.markword;
+
+import com.sun.tools.jdi.LockObject;
+import org.openjdk.jol.info.ClassLayout;
+
+/**
+ * 测试无锁，偏向锁，轻量级锁及重量级锁
+ * MarkWord的变化
+ * @author tianshuo
+ */
+public class LockObjectHeader {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Object a = new Object();
+        /**
+         * 轻量级锁  000 轻量级锁
+         */
+            synchronized (a) {
+                String lockNonLockMarkWord = ClassLayout.parseInstance(a).toPrintable();
+                System.out.println(lockNonLockMarkWord);
+            }
+    }
+
+
+}
