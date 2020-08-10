f8a72e70d20cdce9825254774f554e7c6888c6b6
tianshuo <qwe321a3@126.com>
2020-08-10 11:30:06 +0800

    修改ThreadPoolExecutor测试代码

--- concurrent/src/main/java/com/tianshuo/thread/threadpool/ThreadPoolTest.java
+++ concurrent/src/main/java/com/tianshuo/thread/threadpool/ThreadPoolTest.java
@@ -3,10 +3,10 @@ package com.tianshuo.thread.threadpool;
 import java.util.concurrent.*;
 
 /**
+ * @author tianshuo
  * @Classname ThreadPoolTest
- * @Description TODO
+ * @Description 测试ThreadPoolExecutor
  * @Date 2020/8/1 0001 22:42
- * @Created by Administrator
  */
 public class ThreadPoolTest {
 
@@ -16,9 +16,9 @@ public class ThreadPoolTest {
                 new LinkedBlockingQueue<Runnable>());
 
         for (int i = 0; i < 100; i++) {
-//            executorService.execute(new Thread1());
-            Future<String> str = executorService.submit(new Thread3());
-            System.out.println(str.get());
+            executorService.execute(new Thread1());
+//            Future<String> str = executorService.submit(new Thread3());
+//            System.out.println(str.get());
         }
 
         executorService.shutdown();
