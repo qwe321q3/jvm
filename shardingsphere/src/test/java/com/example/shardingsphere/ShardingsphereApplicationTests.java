c8e3283e730c8edd9b1f8a63b1eb6478f2bac617
tianshuo <qwe321a3@126.com>
2022-03-29 22:08:39 +0800

    解决分片不均匀问题。

--- shardingsphere/src/test/java/com/example/shardingsphere/ShardingsphereApplicationTests.java
+++ shardingsphere/src/test/java/com/example/shardingsphere/ShardingsphereApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.shardingsphere;
 
+import com.example.shardingsphere.entity.Order;
+import com.example.shardingsphere.mapper.OrderMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
 
 @SpringBootTest
 class ShardingsphereApplicationTests {
 
+
+    @Autowired
+    private OrderMapper orderMapper;
+
     @Test
     void contextLoads() {
+
+        for (int i = 1; i <= 20; i++) {
+
+            Order order = new Order();
+            order.setOrderId(Long.valueOf(i));
+            order.setCreateTime(new Date());
+            orderMapper.insert(order);
+        }
+
+
+
+
+
     }
 
 }
